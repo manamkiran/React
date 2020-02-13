fbb381366aafb8ab2c63789c4cb02ffd559294d1
manamkiran <manamkiranyadav@gmail.com>
2020-02-13 21:10:10 +0530

    Adding properties

--- FrontEndMasters/adopt-me/src/App.js
+++ FrontEndMasters/adopt-me/src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = props => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Pet Name"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Thunder")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.petType),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Name",
+      petType: "PetType",
+      breed: "Breed"
+    }),
+    React.createElement(Pet, {
+      name: "Mooncy",
+      petType: "Dog",
+      breed: "Pamerian"
+    }),
+    React.createElement(Pet, {
+      name: "Lulu",
+      petType: "Bird",
+      breed: "Parrot"
+    })
   ]);
 };
 
