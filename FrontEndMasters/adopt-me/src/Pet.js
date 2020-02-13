228bbb4367d935175505885c3ed3ee8d938a8e53
manamkiran <manamkiranyadav@gmail.com>
2020-02-14 00:52:56 +0530

    adding JSX code

--- FrontEndMasters/adopt-me/src/Pet.js
+++ FrontEndMasters/adopt-me/src/Pet.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, petType, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, petType),
-    React.createElement("h2", {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{petType}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
