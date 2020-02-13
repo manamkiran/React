5b6f5160c2448071d9e27d0c19b899d2bb388332
manamkiran <manamkiranyadav@gmail.com>
2020-02-14 00:01:28 +0530

    Adding package.json , .gitignore and configuration files

--- FrontEndMasters/adopt-me/src/App.js
+++ FrontEndMasters/adopt-me/src/App.js
@@ -1,4 +1,4 @@
-const Pet = ({name, petType, breed}) => {
+const Pet = ({ name, petType, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, petType),
