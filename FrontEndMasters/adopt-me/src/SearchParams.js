285a4e46f9b19d720fffeae80bfbd57b1135654d
manamkiran <manamkiranyadav@gmail.com>
2020-02-14 13:55:25 +0530

    adding dropdowns for breed and petTypes

--- FrontEndMasters/adopt-me/src/SearchParams.js
+++ FrontEndMasters/adopt-me/src/SearchParams.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("India");
+
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -14,6 +19,37 @@ const SearchParams = () => {
             placeholder={location}
             onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
