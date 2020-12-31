82d0823daeb09d2b677503c19847194291472cd0
manamkiran <manamkiranyadav@gmail.com>
2021-01-01 03:41:43 +0530

    Added shop page and collection component

--- CompleteReact/crwn-clothing/src/App.js
+++ CompleteReact/crwn-clothing/src/App.js
@@ -1,20 +1,16 @@
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shoppage.component";
+
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
       {/*<HomePage />*/}
     </div>
