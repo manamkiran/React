2c417fa908c785277658d72b29bf71fc296b98b5
manamkiran <manamkiranyadav@gmail.com>
2020-12-31 22:49:14 +0530

    Adding router Logic for MenuItem

--- CompleteReact/crwn-clothing/src/App.js
+++ CompleteReact/crwn-clothing/src/App.js
@@ -1,9 +1,24 @@
-import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS Page</h1>
+  </div>
+);
 
 function App() {
-  return <div><HomePage/></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
+      {/*<HomePage />*/}
+    </div>
+  );
 }
 
 export default App;
