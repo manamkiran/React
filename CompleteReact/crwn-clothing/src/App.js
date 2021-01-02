c696782b5d6a46095df21c6ecca7114f107d2212
manamkiran <manamkiranyadav@gmail.com>
2021-01-02 23:48:37 +0530

    Added signIn Page

--- CompleteReact/crwn-clothing/src/App.js
+++ CompleteReact/crwn-clothing/src/App.js
@@ -4,13 +4,17 @@ import ShopPage from "./pages/shop/shoppage.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
       {/*<HomePage />*/}
     </div>
