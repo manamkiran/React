d98e76f060ffa45991e28cd131442d6279ec1493
manamkiran <manamkiranyadav@gmail.com>
2021-01-04 19:41:16 +0530

    Added Sign Up Page

--- CompleteReact/crwn-clothing/src/firebase/firebase.utils.js
+++ CompleteReact/crwn-clothing/src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   appId: "1:241091719472:web:c45789458ed990248158f5",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userSnapshot = await userRef.get();
+
+  if (!userSnapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error(`error creating user ${error.message}`);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- CompleteReact/crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ CompleteReact/crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
 
 import "./sign-in-and-sign-up.styles.scss";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
-  <div>
+  <div className='sign-in-and-sign-up'>
     <SignIn />
-    SignUp
+    <SignUp />
   </div>
 );
 
