baf0ea656071b0c41259012777b83fc51b7e3eb5
manamkiran <manamkiranyadav@gmail.com>
2021-01-05 16:31:40 +0530

    Added reselect package and started using it for cart related operations

--- CompleteReact/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ CompleteReact/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,20 +5,24 @@ import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
-const CartDropDown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropDown = ({ cartItems }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>Go To CheckOut</CustomButton>
     </div>
-    <CustomButton>Go To CheckOut</CustomButton>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state), //state.cart.cartItems,
+  //cartItems,
 });
 
 export default connect(mapStateToProps)(CartDropDown);
--- CompleteReact/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ CompleteReact/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,37 @@ import React from "react";
 import { connect } from "react-redux";
 
 import "./cart-icon.styles.scss";
-
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
-
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
-  </div>
-);
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+//mapStateToProps is called everytime there is a change in state . If the object/value is same as the previous then the props are not changed
+//and thus the object is not rerendered. Incase we use filter or any other function which creates a new object. Then Since objects are different
+// render is called again.
+/*const mapStateToPros = ({ cart: { cartItems } }) => ({
+  itemCount: selectCartItemsCount(state),
+  itemCount: cartItems.reduce(
+    (accumulator, cartItem) => accumulator + cartItem.quantity,
+    0
+  ),
+});*/
+
+const mapStateToPros = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToPros, mapDispatchToProps)(CartIcon);
