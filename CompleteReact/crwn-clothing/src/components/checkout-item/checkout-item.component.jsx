f327985ac5575c0e3c1c980e6facdd09690d1913
manamkiran <manamkiranyadav@gmail.com>
2021-01-05 23:29:13 +0530

    Added checkOutPage increase decrease Items functionality

--- CompleteReact/crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ CompleteReact/crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./checkout-item.styles.scss";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
-const CheckOutItem = ({ cartItem }) => {
+const CheckOutItem = ({ cartItem, clearItemFromCart, addItem, removeItem }) => {
   const { name, imageUrl, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
@@ -10,11 +16,30 @@ const CheckOutItem = ({ cartItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckOutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItemFromCart: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckOutItem);
