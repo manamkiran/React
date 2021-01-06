3e7d9e947130f1c77f0f01ce2d143562be504a16
manamkiran <manamkiranyadav@gmail.com>
2021-01-06 15:33:46 +0530

    Made few performance optimizations

--- CompleteReact/crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
+++ CompleteReact/crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import "./collections-overview.styles.scss";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -13,8 +15,8 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = ({ shop: { collections } }) => ({
-  collections,
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
